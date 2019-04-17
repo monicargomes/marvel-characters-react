e9706d23b9fd379cadf6d6fb63c64d5fb8b1982f
Monica Gomes <mgomes@mobiquityinc.com>
2019-04-17 14:16:26 +0200

    Updates

---
+++ precache-manifest.2a7eddd1485972d51ec657a45ac23909.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "77a0b8d8b52d7e7dc91bfda68f5d64da",
+    "url": "/marvel-characters-react/static/media/marvel-logo.77a0b8d8.png"
+  },
+  {
+    "revision": "8969edfffff211f7f6671f32c1c10fa1",
+    "url": "/marvel-characters-react/static/media/icomoon.8969edff.svg"
+  },
+  {
+    "revision": "69494041fe3a88b8523cfe7a17e78293",
+    "url": "/marvel-characters-react/static/media/icomoon.69494041.ttf"
+  },
+  {
+    "revision": "6601764f05c79c7227fc187b0646b107",
+    "url": "/marvel-characters-react/static/media/icomoon.6601764f.woff"
+  },
+  {
+    "revision": "279adcc618e6c8d7e2b1a9bcd36390b6",
+    "url": "/marvel-characters-react/static/media/icomoon.279adcc6.eot"
+  },
+  {
+    "revision": "f0b05f08101a513e367e",
+    "url": "/marvel-characters-react/static/js/runtime~main.57ecea38.js"
+  },
+  {
+    "revision": "1e029aaa45b47d1e14c2",
+    "url": "/marvel-characters-react/static/js/main.e307a10a.chunk.js"
+  },
+  {
+    "revision": "bbe391f98a1cd8c75a98",
+    "url": "/marvel-characters-react/static/js/2.fa9bad15.chunk.js"
+  },
+  {
+    "revision": "1e029aaa45b47d1e14c2",
+    "url": "/marvel-characters-react/static/css/main.c8b54d63.chunk.css"
+  },
+  {
+    "revision": "358c901dd38445893a631db4301ff463",
+    "url": "/marvel-characters-react/index.html"
+  }
+];
